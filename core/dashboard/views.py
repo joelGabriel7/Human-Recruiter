25cef0073f530a33b5c95e63ffab64859fd78ce2
Joel German <joelgerman671@gmail.com>
2023-03-28 09:09:57 -0400

    Maquetando las tablas con dataTables

--- core/dashboard/views.py
+++ core/dashboard/views.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 class DashboardView(TemplateView):
-    template_name = 'home.html'
+    template_name = 'dashboard.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
