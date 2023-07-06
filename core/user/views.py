429ce3bd403eb7b59cea71e3c047381df698d988
Joel German <joelgerman671@gmail.com>
2023-07-05 23:08:18 -0400

    Cambio de password funcional

--- core/user/views.py
+++ core/user/views.py
@@ -1,5 +1,5 @@
 import json
-
+from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import Group
 from django.shortcuts import render
@@ -7,7 +7,7 @@ from django.urls import reverse_lazy
 from django.http import JsonResponse, HttpResponseRedirect
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView,View
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View, FormView
 
 from config import settings
 from core.user.forms import UserForm, UserProfileForm
@@ -53,10 +53,10 @@ class UserListView(LoginRequiredMixin, ListView):
 
 class UserCreateView(LoginRequiredMixin,CreateView):
     model = User
-    form_class = UserForm 
+    form_class = UserForm
     template_name = 'user/create.html'
     success_url = reverse_lazy('user:user_list')
-    
+
 
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
@@ -79,18 +79,18 @@ class UserCreateView(LoginRequiredMixin,CreateView):
         context['entity'] = 'Usuarios'
         context['action'] = 'add'
         context['list_url'] = self.success_url
-      
+
         return context
-    
+
 class UserUpdateView(LoginRequiredMixin,UpdateView):
     model = User
-    form_class = UserForm 
+    form_class = UserForm
     template_name = 'user/create.html'
     success_url = reverse_lazy('user:user_list')
-    
+
 
     def dispatch(self, request, *args, **kwargs):
-        self.object = self.get_object() 
+        self.object = self.get_object()
         return super().dispatch(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
@@ -111,9 +111,9 @@ class UserUpdateView(LoginRequiredMixin,UpdateView):
         context['entity'] = 'Usuarios'
         context['action'] = 'edit'
         context['list_url'] = self.success_url
-      
+
         return context
-        
+
 class UserDeleteView(LoginRequiredMixin,DeleteView):
     model = User
     template_name = 'delete.html'
@@ -179,4 +179,51 @@ class UserProfileView(LoginRequiredMixin, UpdateView):
         context['action'] = 'edit'
         context['list_url'] = self.success_url
 
+        return context
+
+
+class UserChangePasswordView(LoginRequiredMixin, FormView):
+    model = User
+    form_class = PasswordChangeForm
+    template_name = 'user/change_password.html'
+    success_url = reverse_lazy('login')
+
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_form(self, form_class=None):
+        form = PasswordChangeForm(user=self.request.user)
+        # Class
+        form.fields['old_password'].widget.attrs['class']= 'form-control'
+        form.fields['new_password1'].widget.attrs['class']= 'form-control'
+        form.fields['new_password2'].widget.attrs['class']= 'form-control'
+        form.fields['new_password2'].widget.attrs['autocomplete']= 'off'
+        # placeholder
+        form.fields['old_password'].widget.attrs['placeholder']= 'Ingrese su nueva contraseña '
+        form.fields['new_password1'].widget.attrs['placeholder']= 'Ingrese su nueva contraseña '
+        form.fields['new_password2'].widget.attrs['placeholder']= 'Repita su contraseña'
+        return form
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            action = request.POST['action']
+            if action == 'edit':
+                form = PasswordChangeForm(user=request.user, data=request.POST)
+                if form.is_valid():
+                    form.save()
+                else:
+                    data['error'] = form.errors
+            else:
+                data['error'] = 'No ha ingresado a ninguna opción'
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Cambiar Contraseña'
+        context['entity'] = 'Password'
+        context['list_url'] = self.success_url
+        context['action'] = 'edit'
         return context
